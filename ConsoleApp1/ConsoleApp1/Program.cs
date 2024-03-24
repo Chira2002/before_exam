4694ed6c16a664de03a841d1ad59b2cc2a620511
Chira2002 <135845862+Chira2002@users.noreply.github.com>
2024-03-24 23:19:46 +0530

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int num01=10;
+            int num02=20;
+            Console.WriteLine("Total is = "+(num01+num02))
+            Console.WriteLine(num01+num02)
             Console.WriteLine("jeewaka chiranjeewa jayasinghe");
         }
     }
